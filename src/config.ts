2058232b345171aa855e89c87d4e3f824d62739a
muhmdtamam <muhamadtamam065@gmail.com>
2026-09-09 10:35:24 +0700

    Delete config.ts

--- src/config.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  BASE_URL: process.env.BASE_URL as string,
-  API_KEY: process.env.API_KEY as string,
-  DB_PATH: "./database/db.json",
-  SESSION_PATH: "./database/session.json",
-  PERSONALITY_PATH: "./database/personality.json"
-}
